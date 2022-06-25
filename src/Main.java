f9b1d6cd6bbe84be85868b654e5e66f5447a64a0
Amit Kumar <amitkumar7566@gmail.com>
2022-06-26 03:46:16 +0530

    added SumOfDigitsOfPositiveInteger

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Java Data Structures and Algorithms");
+
+        SumOfDigitsOfPositiveInteger sumOfDigitsOfPositiveInteger = new SumOfDigitsOfPositiveInteger();
+        var sumOfDigits = sumOfDigitsOfPositiveInteger.sumOfDigits(1134);
+        System.out.println(sumOfDigits);
     }
 }
---
+++ src/SumOfDigitsOfPositiveInteger.java
@@ -0,0 +1,12 @@
+public class SumOfDigitsOfPositiveInteger {
+
+    public int sumOfDigits(int number) {
+
+        if (number == 0 || number < 0) {
+            return 0;
+        }
+
+        return number % 10 + sumOfDigits(number/10);
+
+    }
+}
