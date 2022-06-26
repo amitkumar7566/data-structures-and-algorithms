525f02bfa098cd964b0dce3a4a16811c3e780732
Amit Kumar <amitkumar7566@gmail.com>
2022-06-26 17:45:21 +0530

    added GCD

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,11 @@ public class Main {
         int number3 = 5;
         var powerValue = power.power(2, 4);
         System.out.println("\nThe value of " + number2 + " to the power " + number3 + ": " + powerValue);
+
+        GCD gcd = new GCD();
+        int number4 = 48;
+        int number5 = 36;
+        var gcdValue = gcd.gcd(number4, number5);
+        System.out.println("\nThe Greatest Common Divisor (GCD) of " + number4 + " and " + number5 + ": " + gcdValue);
     }
 }
